d36460ffe1f97f174d91ed490f1ee79f55b6e9b2
Reya <ylian06@qq.com>
2022-08-07 17:59:47 +0800

    初始化滚动组件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pinxx';
+  selectIndex = -1;
+  menus: TopMenu[] = [
+    {
+      title: '热门',
+      link: ''
+    },
+    {
+      title: '男装',
+      link: ''
+    },
+    {
+      title: '百货',
+      link: ''
+    },
+    {
+      title: '运动',
+      link: ''
+    },
+    {
+      title: '手机   ',
+      link: ''
+    },
+    {
+      title: '家纺',
+      link: ''
+    },
+    {
+      title: '食品',
+      link: ''
+    },
+    {
+      title: '美妆',
+      link: ''
+    },
+    {
+      title: '家电',
+      link: ''
+    },
+    {
+      title: '粮油',
+      link: ''
+    },
+  ]
+
+  dict: Dict = {
+    a: '1',
+    b: '2'
+  }
+
+  constructor() {
+    console.log(this.dict['a']);
+  }
+
+  add: AddFunc = (x, y) => x + y;
+
+  handleSelection(i:number){
+    this.selectIndex = i;
+  }
+}
+// interface - 对象类型
+interface TopMenu {
+  title: string;
+  link?: string;
+}
+
+// interface - 函数类型
+interface AddFunc {
+  (x: number, y: number): number;
+}
+
+// interface - 索引类型
+interface Dict {
+  [key: string]: string;
 }
---
+++ src/app/scrollable-tab/scrollable-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scrollable-tab',
+  templateUrl: './scrollable-tab.component.html',
+  styleUrls: ['./scrollable-tab.component.scss']
+})
+export class ScrollableTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
