5d0a75d2b408002ab8eea306cc0542ce8a21ecb8
Reya <ylian06@qq.com>
2022-09-11 18:40:09 +0800

    组件生命周期

--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,12 +1,22 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-scrollable-tab',
   templateUrl: './scrollable-tab.component.html',
   styleUrls: ['./scrollable-tab.component.scss'],
 })
-export class ScrollableTabComponent implements OnInit {
+/**
+ * 接口是可选的，也就是说只要有类似 ngOnInit 这样的方法存在
+ * 生命周期的钩子函数还是正常执行
+ * 但建议实现接口，好处一个是不会误删除某个钩子函数
+ * 另一个是对组件涉及到哪些生命周期一目了然
+ */
+export class ScrollableTabComponent implements OnInit,OnChanges {
   @Input() menus: TopMenu[] = [];
+  @Input() backgroundColor = '#fff'
+  @Input() titleColor = 'blue'
+  @Input() titleActiveColor = 'yellow'
+  @Input() indicatorColor = 'brown'
   @Output() tabSelected = new EventEmitter();
 
   selectIndex = -1;
@@ -14,9 +24,14 @@ export class ScrollableTabComponent implements OnInit {
     a: '1',
     b: '2',
   };
-
+  /**
+   * 构造函数永远首先被调用 
+   */
   constructor() {
-    console.log(this.dict['a']);
+    console.log('组件构造调用');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('组件输入属性改变',changes)
   }
 
   add: AddFunc = (x, y) => x + y;
@@ -26,7 +41,12 @@ export class ScrollableTabComponent implements OnInit {
     this.tabSelected.emit(this.menus[this.selectIndex]);
   }
 
-  ngOnInit(): void {}
+  /**
+   * 组件初始化完成，在这个函数中，我们可以安全的使用组件的属性和方法
+   */
+  ngOnInit(): void {
+    console.log('组件初始化')
+  }
 }
 // interface - 对象类型
 export interface TopMenu {
