9a06de8135f409e13e3f61250a051d9491a558b5
Reya <ylian06@qq.com>
2022-10-09 00:06:46 +0800

    4.13:依赖注入

--- src/app/home/components/home-grand/home-grand.component.ts
+++ src/app/home/components/home-grand/home-grand.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, InjectionToken, Injector, OnInit } from '@angular/core';
+import { ProductModule } from 'src/app/product/product.module';
+
+// @Injectable()
+// class Product {
+//   constructor(private name: string) {}
+// }
+// @Injectable()
+// class PurchaseOrder {
+//   private amount: number;
+//   constructor(private product: Product) {
+//     // this.product = new Product('大米手机')
+//   }
+// }
 
 @Component({
   selector: 'app-home-grand',
@@ -12,17 +25,40 @@ export class HomeGrandComponent implements OnInit {
     model: 'm',
     type: 'mobile phone',
   };
-  date:Date;
+  date: Date;
 
   constructor() {}
 
   ngOnInit() {
-    this.date  = this.minusDays(new Date(), 60);
+    this.date = this.minusDays(new Date(), 60);
+    // const product = new Product('大米先生');
+    // const purchaseOrder = new PurchaseOrder(product);
+    // const token = new InjectionToken<string>('baseUrl')
+    // const injector = Injector.create({
+    //   providers: [
+    //     {
+    //       provide: Product,
+    //       // useClass: Product,
+    //       useFactory: () => {
+    //         return new Product('大米先生');
+    //       },
+    //     },
+    //     {
+    //       provide: PurchaseOrder,
+    //       useClass: PurchaseOrder,
+    //       deps: [Product],
+    //     },
+    //     {
+    //       provide: token,
+    //       useValue:'http://localhost:8080'
+    //     }
+    //   ],
+    // });
   }
 
-  minusDays(date:Date, days:number){
-     const result = new Date(date);
-     result.setDate(result.getDate() - days);
-     return result
+  minusDays(date: Date, days: number) {
+    const result = new Date(date);
+    result.setDate(result.getDate() - days);
+    return result;
   }
 }
